6e032a4b26ecb004df093fac24552674ac359b4b
Mike White <myquite@gmail.com>
2022-06-23 10:40:34 -0500

    changed to a modules structure, added new function

---
+++ modules/helpers.js
@@ -0,0 +1,13 @@
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x + y;
+}
+
+function greet(name) {
+  return `Hello, ${name}`;
+}
+
+export { add, subtract, greet };
--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-import { test, expect } from "./tests/test.js";
-
-function add(x, y) {
-  return x + y;
-}
-
-function subtract(x, y) {
-  return x * y;
-}
+import { test, expect } from "./modules/test.js";
+import { add, subtract, greet } from "./modules/helpers.js";
 
 test(`adds two values`, add, () => {
   expect(add(1, 2)).toBe(3);
@@ -14,4 +7,8 @@ test(`adds two values`, add, () => {
 
 test(`subtracts two values`, subtract, () => {
   expect(subtract(10, 5)).toBe(5);
+});
+
+test(`greets a name`, greet, () => {
+  expect(greet("John")).toBe("Hello, John");
 });
